ff224305b05254cc6785ef2db980895a0b9a4856
Javier Juarez <javier.juarez@gmail.com>
2010-11-15 22:14:58 +0100

    Cambio en la gestion del timeout

--- lib/check_proxies/url_checker.rb
+++ lib/check_proxies/url_checker.rb
@@ -3,7 +3,7 @@ module CheckProxies
     
     def check
       Logger::instance.debug( "Checking: #{@proxy_uri} -> #{@url}..." )
-      Net::HTTP::Proxy( @proxy_uri.host, @proxy_uri.port ).start( @url ) { |http| ( http.request( @request ) == Net::HTTPSuccess ) }
+      Net::HTTP::Proxy( @proxy_uri.host, @proxy_uri.port ).start( @url ) { |http| return true if http.request( @request ) == Net::HTTPSuccess }
     rescue => e
       Logger::instance.error e.message                            
       false 
